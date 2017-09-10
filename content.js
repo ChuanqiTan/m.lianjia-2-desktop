935b2027501d7a54d296cef726d41156f630ac7c
tanchuanqi <chuanqi.tan@gmail.com>
2017-09-10 15:41:02 +0800

    not jump in non detail page

--- content.js
+++ content.js
@@ -1,12 +1,13 @@
-console.log("Jump to desktop page");
-console.log(location.href);
-
 var currentUrl = location.href;
-var pstr = "m.lianjia.com/bj/";
-var i = currentUrl.indexOf(pstr);
-if (i != -1) {
-    var nextUrl = "https://bj.lianjia.com/" + currentUrl.substr(i + pstr.length);
-    console.log(nextUrl);
-    window.location = nextUrl;
+if (currentUrl.match("/ershoufang/\\d+\.html")) {
+    console.log("Matched:" + currentUrl);
+    var pstr = "m.lianjia.com/bj/";
+    var i = currentUrl.indexOf(pstr);
+    if (i != -1) {
+        var nextUrl = "https://bj.lianjia.com/" + currentUrl.substr(i + pstr.length);
+        console.log(nextUrl);
+        window.location = nextUrl;
+    }
 }
+
 
